f281dabdd94376ba8a71d4d4f816710b09fab787
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-07 17:09:38 -0300

    tercera actualización react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RestaurantApp } from './RestaurantApp';
 import 'antd/dist/antd.css';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <React.StrictMode>
+     <BrowserRouter>
       <RestaurantApp />
     </BrowserRouter>
-)
+  </React.StrictMode>
+);
